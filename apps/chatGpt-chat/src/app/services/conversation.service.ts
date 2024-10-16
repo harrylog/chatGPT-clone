c63a523b93605c33c27a57f1a2f018f42c35fd73
harry log <harry8log@gmail.com>
2024-10-16 10:59:56 +0300

    update

--- apps/chatGpt-chat/src/app/services/conversation.service.ts
+++ apps/chatGpt-chat/src/app/services/conversation.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConversationService {
+  private apiUrl = 'https://api.openai.com/v1/chat/completions';
+  openaiApiKey =
+    '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';
+  constructor(private http: HttpClient) {}
+  sendMessage(message: string): Observable<string> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.openaiApiKey}`,
+    });
 
-  constructor() { }
+    const body = {
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: message }],
+      temperature: 0.7,
+    };
+
+    return this.http.post<string>(this.apiUrl, body, { headers });
+  }
 }
